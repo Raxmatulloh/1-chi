58f3d8f729ba8cd28ed35f30198b49b8e28823d1
Raxmatulloh <92166503+Raxmatulloh@users.noreply.github.com>
2021-10-08 18:57:12 +0500

    yukladim

---
+++ script.js
@@ -0,0 +1,8 @@
+function login() {
+    var ism=document.getElementById('ism').value;
+    var Familya=document.getElementById('familya').value;
+    var email=document.getElementById('email').value;
+    var parol=document.getElementById('parol').value;
+    document.write(ism+Familya+email+parol)
+    
+}
